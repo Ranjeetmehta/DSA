4108cd34b098c5cf4cc79e61d06de11034e55eee
RANJEET KUMAR MEHTA <121413963+Ranjeetmehta@users.noreply.github.com>
2023-02-10 16:11:40 +0530

    Create Minimum element in BST.java

---
+++ java/BST/Minimum element in BST.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int val;
+    Node left, right;
+    Node(int val){
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
+class BST{
+    Node root = null;
+    BST(){
+        
+    }
+    BST(Node root){
+        root=root;
+    }
+    Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.val == val)
+            return root;
+        if(val < root.val){
+            root.left = insert(root.left, val);
+        }else{
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    void print(Node root){
+        System.out.print(root.val + " ");
+        if(root.left != null){
+            print(root.left);
+        }
+        if(root.right != null){
+            print(root.right);
+        }
+    }
+}
+
+class Solution{
+    public int minimumNode(Node root){
+        // WRITE YOUR CODE HERE      
+		if(root==null) return 0;
+		Node current=root;
+		while(current.left!=null){
+			current=current.left;
+		}
+		return (current.val);
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        BST t = new BST();
+        for(int i = 0; i < n; i++){
+            t.root = t.insert(t.root, input.nextInt());
+        }
+        Solution obj=new Solution();
+        int result= obj.minimumNode(t.root);
+        System.out.print(result);
+        
+    }
+}

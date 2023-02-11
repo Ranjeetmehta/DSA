75b6b4b5f36fa4fe892d2c20cfe3c5ea62a932fd
RANJEET KUMAR MEHTA <121413963+Ranjeetmehta@users.noreply.github.com>
2023-02-11 19:22:21 +0530

    Create two sum.java

---
+++ java/two pointer/two sum.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    
+    public static int[] twoSum(int[] A, int target) {
+       // Your code here
+		int n=A.length;
+		int l=0;
+		int r=n-1;
+		int[] ans=new int[2];
+		while(l<r){
+		int sum=A[l]+A[r];
+		if(sum==target){
+			ans[0]=l+1;
+			ans[1]=r+1;
+			return ans;
+		}
+			else if(sum<target) l++;
+		else r--;
+		}
+	return ans;
+    }
+	
+    public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+        long n = Long.parseLong(br.readLine().trim());
+        String inputLine[] = br.readLine().trim().split(" ");
+        int[] arr = new int[(int)n];
+        for(int i=0; i<n; i++)arr[i]=Integer.parseInt(inputLine[i]);
+        int m = Integer.parseInt(br.readLine().trim());
+        int[] ans = (twoSum(arr, m));
+        System.out.println(ans[0] + " " + ans[1]);
+	}
+}
+
+
+

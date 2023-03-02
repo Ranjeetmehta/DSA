2635d86451df63b85012402044f5cb0385d6e8b6
RANJEET KUMAR MEHTA <121413963+Ranjeetmehta@users.noreply.github.com>
2023-03-02 10:36:20 +0530

    Create Job Sequencing Problem.java

---
+++ java/Greedy/Job Sequencing Problem.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+        String inputLine[] = br.readLine().trim().split(" ");
+    
+        int n = Integer.parseInt(inputLine[0]);
+        Job[] arr = new Job[n];
+        inputLine = br.readLine().trim().split(" ");
+        
+        //adding id, deadline, profit
+        for(int i=0, k=0; i<n; i++){
+            arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+        }
+        
+        Solution ob = new Solution();
+        
+        //function call
+        int[] res = ob.solve(arr, n);
+        System.out.println (res[0] + " " + res[1]);
+    
+    }
+}
+
+class Solution
+{
+    
+    int[] solve(Job arr[], int n)
+    {
+        // Your code here
+		boolean[] days=new boolean[n+1];
+		int cnt=0;
+		int totalProfit=0;
+		Arrays.sort(arr,(a,b)->{
+			return b.profit-a.profit;
+		});
+					for(int i=0;i<n;i++){
+			int bestPossibleDay=arr[i].deadline;
+			while(bestPossibleDay>0 && days[bestPossibleDay]==true) bestPossibleDay--;
+			if(bestPossibleDay==0) continue;
+			days[bestPossibleDay]=true;
+			cnt++;
+			totalProfit+=arr[i].profit;
+			
+					}
+		int[] ans={cnt,totalProfit};
+		return ans;
+
+    }
+}

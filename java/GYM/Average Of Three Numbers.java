2e9f08be4ed16666e60c92a16d45be7cbcf519f8
RANJEET KUMAR MEHTA <121413963+Ranjeetmehta@users.noreply.github.com>
2023-03-07 16:31:25 +0530

    Create Average Of Three Numbers.java

---
+++ java/GYM/Average Of Three Numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+		Scanner sc=new Scanner(System.in);
+		Double a=sc.nextDouble();
+		Double b= sc.nextDouble();
+		Double c=sc.nextDouble();
+		double average=(a+b+c)/3;
+		System.out.println(average);
+	}
+}

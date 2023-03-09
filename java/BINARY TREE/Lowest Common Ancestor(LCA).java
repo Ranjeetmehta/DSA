67de6c086e40f3b0710a13ea5f70b24a90228f49
RANJEET KUMAR MEHTA <121413963+Ranjeetmehta@users.noreply.github.com>
2023-03-09 20:16:55 +0530

    Create Lowest Common Ancestor(LCA).java

---
+++ java/BINARY TREE/Lowest Common Ancestor(LCA).java
@@ -0,0 +1,101 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+
+
+class Main {
+    static Node buildTree(String str) {
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if (!currVal.equals("-1")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if (i >= ip.length) break;
+            currVal = ip[i];
+            if (!currVal.equals("-1")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        Node root = buildTree(s);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        Solution g = new Solution();
+        Node ans = g.findLCA(root,x,y);
+        System.out.println(ans.data);
+    }
+}
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+    public static Node findLCA(Node node,int n1,int n2) {
+       // write code here
+		if(node==null) return null;
+		if(node.data==n1) {
+			return node;
+		}
+		if(node.data==n2){
+			return node;
+		}
+		Node foundInLeft=findLCA(node.left,n1,n2);
+		Node foundInRight=findLCA(node.right,n1,n2);
+		if(foundInLeft!=null && foundInRight!=null){
+			return node;
+		}
+		if(foundInLeft!=null) {
+			return foundInLeft;
+		}
+		if(foundInRight!=null){
+			return foundInRight;
+			
+		}
+		return null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

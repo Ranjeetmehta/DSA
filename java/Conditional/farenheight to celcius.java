301f53ab340028bd0e4ac545af26790b6dd94076
RANJEET KUMAR MEHTA <121413963+Ranjeetmehta@users.noreply.github.com>
2023-03-10 16:03:11 +0530

    Create farenheight to celcius.java

---
+++ java/Conditional/farenheight to celcius.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+		Scanner sc=new Scanner(System.in);
+		Double f=sc.nextDouble();
+		Double c=((f-32)*5)/9;
+		System.out.println(c);
+	}
+}

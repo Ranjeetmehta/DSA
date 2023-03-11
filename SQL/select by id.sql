257c3780649ae3b3cae6addb6f1d2d4bcb071a57
RANJEET KUMAR MEHTA <121413963+Ranjeetmehta@users.noreply.github.com>
2023-03-11 08:02:02 +0530

    Create select by id.sql

---
+++ SQL/select by id.sql
@@ -0,0 +1,13 @@
+Query all columns for a city in CITY with the ID 1661.
+
+The CITY table is described as follows:
+
+Field	Type
+ID	NUMBER
+NAME	VARCHAR2(17)
+COUNTRY CODE	VARCHAR2(3)
+DISTRICT	VARCHAR2(20)
+POPULATION	NUMBER
+
+select*from city
+where id='1661'

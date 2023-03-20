15bd944a766b263143960126c04c899e0983ca7b
RANJEET KUMAR MEHTA <121413963+Ranjeetmehta@users.noreply.github.com>
2023-03-20 08:02:13 +0530

    Create Convert a given string into lowercase.java

---
+++ java/String/Convert a given string into lowercase.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+		Scanner sc=new Scanner(System.in);
+		String str=sc. nextLine();
+		System.out.println(str.toLowerCase());
+	}
+}
